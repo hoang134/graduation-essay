4022266d9a8f3f5566331553f3560d333bdba526
hoang134 <hoang13499.kshb@gmail.com>
2020-08-26 00:08:45 +0700

    fixed route

--- routes/web.php
+++ routes/web.php
@@ -21,16 +21,16 @@ Route::get('/','HomeController@index')->name('home');
 
 Route::prefix('admin')->middleware('CheckRole')->group(function () {
         Route::get('post','PostController@index')->name('post');
-        Route::get('create','PostController@create')->name('post.create');
-        Route::get('edit/{id}','PostController@edit')->name('post.edit');
-        Route::get('delete','PostController@delete')->name('post.delete');
-        Route::post('save/{id?}','PostController@save')->name('post.save');
+        Route::get('post/create','PostController@create')->name('post.create');
+        Route::get('post/edit/{id}','PostController@edit')->name('post.edit');
+        Route::get('post/delete','PostController@delete')->name('post.delete');
+        Route::post('post/save/{id?}','PostController@save')->name('post.save');
 
         Route::get('user','UserController@index')->name('user');
-        Route::get('user','UserController@create')->name('user.create');
-        Route::get('user','UserController@edit')->name('user.edit');
+        Route::get('user/create','UserController@create')->name('user.create');
+        Route::get('user/edit','UserController@edit')->name('user.edit');
         Route::get('user/{id}','UserController@delete')->name('user.delete');
-        Route::post('save/{id?}','UserController@save')->name('post.save');
+        Route::post('user/save/{id?}','UserController@save')->name('user.save');
 });
 Route::get('login','LoginController@getLogin')->name('login');
 Route::post('login','LoginController@postLogin')->name('login');
